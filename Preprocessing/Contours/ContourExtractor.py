fd17abbbe3977274c27501a31de790675bf83f28
Valery <schwartzvalery@gmail.com>
2017-05-13 13:54:56 +0500

    fix thing with wrong contour recognition

--- Preprocessing/Contours/ContourExtractor.py
+++ Preprocessing/Contours/ContourExtractor.py
@@ -13,20 +13,9 @@ def extract_all_countours(image):
     img = cv2.imread(image, cv2.CV_8UC1)
     img = cv2.GaussianBlur(img, (3,3), 0);
     img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-    cv2.bitwise_not(img, img)
+    cv2.bitwise_not(img, img)#important!
     img2, contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS)
     return img2, contours, hierarchy
-'''
-def write_countours(img, contours, name, dest):
-    idx = 0
-    for cnt in contours:
-        idx += 1
-        x, y, w, h = cv2.boundingRect(cnt)
-        roi = img[y:y + h, x:x + w]
-        cv2.imwrite(os.path.join(dest, str(idx) + '_' + name[:-4].replace("_alltogether","") + '.png'), cv2.bitwise_not(roi, roi))
-'''
-def merge_contours(cnt1, cnt2, img):
-    return [(cnt1[0][0],cnt2[0][1]), cnt1[1], np.concatenate((cnt1[2],cnt2[2]))]
 
 def find_contour_coordinates(image, cnts, hierarchy):
     coordinates = []
@@ -40,10 +29,33 @@ def find_contour_coordinates(image, cnts, hierarchy):
         coordinates.append([(cX,cY), c, box])
     return coordinates
 
+def write_contour(img, image, contour):
+
+    x, y, w, h = cv2.boundingRect(contour)
+    '''
+    pts1 = np.float32([x,y])
+    '''
+    roi = img[y:y + h, x:x + w]
+    cv2.imwrite(image, roi)
+
+    #cv2.cv2.resize()
+
+
+def get_nearest_graph(coord_cnts, near_dest):
+    length = len(coord_cnts)
+    nearest = [[]] * length
+    for i in range(0, length):
+        nearest[i].append(i)
+        for k in range(0, length - 1):
+            if find_distance_between(coord_cnts[i], coord_cnts[k]) <= near_dest:
+                nearest[i].append(k)
+    return nearest
+
 def find_distance_between(coord_cnt1, coord_cnt2):
     return dist.euclidean((coord_cnt1[0][0],coord_cnt1[0][1]), (coord_cnt2[0][0], coord_cnt2[0][1]))
 
-def find_average_distances(coord_cnts):
+
+def find_average_distance(coord_cnts):
     sum = 0
     for c in coord_cnts:
         for k in coord_cnts:
@@ -52,4 +64,12 @@ def find_average_distances(coord_cnts):
 
 def midpoint(ptA, ptB):
 	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
-
+'''
+def write_countours(img, contours, name, dest):
+    idx = 0
+    for cnt in contours:
+        idx += 1
+        x, y, w, h = cv2.boundingRect(cnt)
+        roi = img[y:y + h, x:x + w]
+        cv2.imwrite(os.path.join(dest, str(idx) + '_' + name[:-4].replace("_alltogether","") + '.png'), cv2.bitwise_not(roi, roi))
+'''
--- TestingSampleGenerator.py
+++ TestingSampleGenerator.py
@@ -5,21 +5,19 @@ import Preprocessing.TTF.FontManipulator as manipulator
 import Preprocessing.TTF.FontPainter as painter
 import Preprocessing.Contours.ContourExtractor as Contour
 import cv2
+import numpy as np
 
 if __name__ == "__main__":
     image = "sample.png"
     img, contours, hierarchy = Contour.extract_all_countours(image)
     coordinates = Contour.find_contour_coordinates(img, contours, hierarchy)
-    average = Contour.find_average_distances(coordinates)
-    resulting_contours = []
-    '''
-    for i in list(coordinates):
-        for k in coordinates:
-            if Contour.find_distance_between(i, k) <= average:
-                cn = Contour.merge_contours(i, k, img)
-                coordinates.remove(i)
-                coordinates.remove(k)
-                coordinates.append(cn)
-    '''
+    average = Contour.find_average_distance(coordinates)
+    nearest = Contour.get_nearest_graph(coordinates, average)
+    idx = 0
+    for c in contours:
+      #  Contour.write_contour( img, str(idx) + "result.png", c)
+        idx+=1
+
+
     print("done")
 

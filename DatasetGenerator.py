fbc422479bd98ae027f841f7874657917509b4a7
Valery <schwartzvalery@gmail.com>
2017-05-18 01:08:06 +0500

    testing images labeled

--- DatasetGenerator.py
+++ DatasetGenerator.py
@@ -12,6 +12,8 @@ import csv
 
 def generate_labeled_dataset():
     return 0
+def generate_labled_csv(image_data_folder, csvfile):
+    return 0
 
 def generate_classes_csv(raw_data_folder, csvfile):
     with open(csv_file, 'w') as csvfile:
@@ -24,32 +26,38 @@ def generate_classes_csv(raw_data_folder, csvfile):
             name = os.path.splitext(files[i])[0]
             writer.writerow({'filename': name, 'class': str(i)})
 
-def generate_labled_csv(image_data_folder, csvfile):
-    return 0
-'''
 def lable_files(image_data_folder, csvfile):
     files = [f for f in os.listdir(image_data_folder) if os.path.isfile(os.path.join(image_data_folder, f)) and f.endswith(".png")]
-    with open(csv_file, 'r') as csvfile:
+    for i in range(0, len(files)):
+        filename = files[i].split("_")
+        file_class = get_class(filename[1], csvfile)
+        new_name = os.path.join(image_data_folder, str(i) + "_" + file_class + ".png")
+        os.rename(os.path.join(image_data_folder, files[i]), new_name)
+        print(files[i], " ", os.path.basename(new_name), " ",file_class, "\n")
+
+def get_class(name, csvfile):
+    with open(csvfile, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
-'''
+        for row in reader:
+            if name == row['filename']:
+                return row['class']
+
 def image_to_feature_vector(image):
     return image.flatten()
 
-def get_lable(image):
-    return None
-
-def write_metric_to_file(metric, filename):
-    return 0
 
 if __name__ == "__main__":
     config = cp.ConfigParser()
     config.read_file(open('config.ini'))
     TTFData = config.get('Directories', 'ttfdata')
     csv_file = config.get('Datasets', 'classes')
+    image_folder = config.get('Directories', 'learningsamplefolder')
     dataset_folder = config.get('Directories', 'datasetfolder')
+
     if not os.path.exists(TTFData) or not os.path.exists(dataset_folder):
         if not os.path.exists(TTFData):
             os.makedirs(TTFData)
         if not os.path.exists(dataset_folder):
             os.makedirs(dataset_folder)
     generate_classes_csv(TTFData, csv_file)
+    lable_files(image_folder,csv_file)
